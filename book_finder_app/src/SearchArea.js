3cb119790125984cb22568a3a98507f88bb9735f
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-12-30 02:36:03 +0600

    SearchBox and Button Created

--- book_finder_app/src/SearchArea.js
+++ book_finder_app/src/SearchArea.js
@@ -3,7 +3,10 @@ import React from "react";
 const SearchArea = () => {
   return (
     <div>
-      <h2>SearchArea</h2>
+      <form action="">
+        <input type="text"></input>
+        <input type="submit" value="Search"></input>
+      </form>
     </div>
   );
 };
