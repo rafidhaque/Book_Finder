69d453c3adb27125536230c087bf033df3948975
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-12-30 10:53:22 +0600

    search field update

--- book_finder_app/src/Books.js
+++ book_finder_app/src/Books.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchArea from "./SearchArea";
 
-const Books = () => {
-  return (
-    <div>
-      <SearchArea />
-    </div>
-  );
-};
+class Books extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      searchField: "",
+    };
+  }
+
+  handleSearch = (e) => {
+    console.log(e.target.value);
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchArea handleSearch={this.handleSearch} />
+      </div>
+    );
+  }
+}
 
 export default Books;
--- book_finder_app/src/SearchArea.js
+++ book_finder_app/src/SearchArea.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const SearchArea = () => {
+const SearchArea = (props) => {
   return (
     <div className="search-area">
       <form action="">
-        <input type="text"></input>
+        <input type="text" onChange={props.handleSearch}></input>
         <input type="submit" value="Search" id="button"></input>
       </form>
     </div>
