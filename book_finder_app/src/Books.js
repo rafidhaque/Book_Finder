7e2153ceb99bc769438959bcd723d8907979540b
rafidhaque <rafidhaque@iut-dhaka.edu>
2020-12-30 11:21:44 +0600

    books setStated

--- book_finder_app/src/Books.js
+++ book_finder_app/src/Books.js
@@ -22,7 +22,7 @@ class Books extends Component {
       .get("https://www.googleapis.com/books/v1/volumes")
       .query({ q: this.state.searchField })
       .then((data) => {
-        console.log(data);
+        this.setState({ books: [...data.body.items] });
       });
   };
 
